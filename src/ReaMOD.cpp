4a1e59ccdb41e39e1371d4beec733f63b37963d5
Daniel Dehaan <danielrdehaan@gmail.com>
2025-10-02 10:36:40 -0500

    Enabling debug messages by default.

--- src/ReaMOD.cpp
+++ src/ReaMOD.cpp
@@ -47,7 +47,7 @@ static int actionIDSearchForFmodEvent = 0;
 static int actionIDTriggerSelectedEvent = 0;
 static int actionIDToggleDebugOnOff = 0;
 
-bool debugMessages = false;
+bool debugMessages = true;
 
 
 // ImGui context
