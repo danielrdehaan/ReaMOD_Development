28a50615cc1b2ffcb1f3296d31462561e0f73799
Daniel Dehaan <danielrdehaan@gmail.com>
2025-10-02 10:30:51 -0500

    Adding debug toggle to settings.

--- src/ReaMOD.cpp
+++ src/ReaMOD.cpp
@@ -3235,6 +3235,9 @@ void RenderGUI() {
         ImGui::Checkbox(reaMOD_Main_ImGui_Context, "Move edit to end of inserted item.", &moveCursorAfterInsert);
         ImGui::Checkbox(reaMOD_Main_ImGui_Context, "Update item length from last time-selection insert.", &updateItemInsertionLength);
 
+        // Add the checkbox for toggling debug messages in Reaper
+        ImGui::Checkbox(reaMOD_Main_ImGui_Context, "Enable Debugs Messages", &debugMessages);
+
         ImGui::Separator(reaMOD_Main_ImGui_Context);
         ImGui::Text(reaMOD_Main_ImGui_Context, "ReaMOD v0.1");
         ImGui::Text(reaMOD_Main_ImGui_Context, "Created by Daniel Dehaan");
