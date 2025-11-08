d4922f1cfad36db76acae32988de74f1f8c1db26
Daniel Dehaan <danielrdehaan@gmail.com>
2025-11-08 06:38:15 -0600

    Fixed typo

--- src/ReaMOD.cpp
+++ src/ReaMOD.cpp
@@ -4026,7 +4026,7 @@ void RenderGUI() {
         }
 
         ImGui::Separator(reaMOD_Main_ImGui_Context);
-        ImGui::Text(reaMOD_Main_ImGui_Context,"Beta V1.0")
+        ImGui::Text(reaMOD_Main_ImGui_Context,"Beta V1.0");
 
         // Pop style colors after End() but before cleanup
         // PopReaMODInterfaceStyle(reaMOD_Main_ImGui_Context);
